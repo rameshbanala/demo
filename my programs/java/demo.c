b1ed1f09d8d38a87d24da5751b3ee2f5f5aa14af
ramesh <rameshbanalab@gmail.com>
2024-05-06 22:54:15 +0530

    new version

---
+++ my programs/java/demo.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d",3);
+    printf("%d",3);
+}
---
+++ my programs/java/exam.java
@@ -0,0 +1,5 @@
+class Exams{
+	public staic void main(Strings[] args){
+		System.out.ptrintln("ramesh");
+	}
+}
